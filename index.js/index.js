10c0920533be74c289ce4c40fb5b92f845de5142
nightsha <130249622+nightsha@users.noreply.github.com>
2023-11-13 20:13:31 -0600

    MoreCorrections

--- index.js/index.js
+++ index.js/index.js
@@ -1,8 +1,8 @@
-import { Triangle, Circle, Square } from './shape.js';
+// export { Triangle, Circle, Square } from './shape.js';
 
-const triangle = new Triangle();
-const circle = new Circle();
-const square = new Square();
+// const triangle = new Triangle();
+// const circle = new Circle();
+// const square = new Square();
 
 // Further code to use these shapes as needed in your application
 
@@ -13,6 +13,10 @@ const myTriangle = new Triangle(0, 0, 5, 4);
 const myCircle = new Circle(2, 3, 6);
 const mySquare = new Square(1, 1, 7);
 
-myTriangle.calculateArea(); // Call the specific method for Triangle
-myCircle.calculateArea();   // Call the specific method for Circle
-mySquare.calculateArea();   // Call the specific method for Square
+const triangleArea = myTriangle.calculateArea(); // Call the specific method for Triangle
+const circleArea = myCircle.calculateArea();   // Call the specific method for Circle
+const squareArea = mySquare.calculateArea();   // Call the specific method for Square
+
+console.log(triangleArea);
+console.log(circleArea)
+console.log(squareArea)
